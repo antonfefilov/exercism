7ed990b3e63ea259be80cc3e6e52b305c93fbafd
Anton Fefilov <anton.fefilov@macrocoders.com>
2020-01-19 12:45:59 +0400

    Solve the problem

--- c/darts/src/darts.c
+++ c/darts/src/darts.c
@@ -1,6 +1,26 @@
 #include "darts.h"
+#include <math.h>
+
+static float radius(coordinate_t landing_position)
+{
+  return sqrt(pow(landing_position.x, 2) + pow(landing_position.y, 2));
+}
 
 int score(coordinate_t landing_position)
 {
-  return landing_position.x + landing_position.x;
+  const float RAD = radius(landing_position);
+
+  if(RAD > 10) {
+    return 0;
+  }
+
+  if(RAD > 5) {
+    return 1;
+  }
+
+  if(RAD > 1) {
+    return 5;
+  }
+
+  return 10;
 }
