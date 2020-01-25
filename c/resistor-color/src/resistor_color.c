e0132a19b10b5a1fd26cb1a9e0a1b703eef0d3fa
Anton Fefilov <anton.fefilov@macrocoders.com>
2020-01-26 02:01:11 +0400

    Solve the issue

--- c/resistor-color/src/resistor_color.c
+++ c/resistor-color/src/resistor_color.c
@@ -1,1 +1,17 @@
 #include "resistor_color.h"
+#include <stdio.h>
+
+const resistor_band_t * colors()
+{
+  static const resistor_band_t col[10] = {
+    BLACK, BROWN, RED, ORANGE, YELLOW,
+    GREEN, BLUE, VIOLET, GREY, WHITE
+  };
+
+  return col;
+}
+
+int color_code(resistor_band_t color)
+{
+  return colors()[color];
+}
