79cbf9aca2d05b6b63768806f48980e923431372
Anton Fefilov <anton.fefilov@macrocoders.com>
2020-01-26 21:10:09 +0400

    Improve last issue code

--- c/resistor-color-duo/src/resistor_color_duo.c
+++ c/resistor-color-duo/src/resistor_color_duo.c
@@ -4,18 +4,5 @@
 
 uint16_t color_code(resistor_band_t colors[])
 {
-  int code;
-  char color_1[2];
-  char color_2[2];
-  char codeStr[3];
-
-  sprintf(color_1, "%d", colors[0]);
-  sprintf(color_2, "%d", colors[1]);
-
-  strcpy(codeStr, color_1);
-  strcat(codeStr, color_2);
-
-  sscanf(codeStr, "%d", &code);
-
-  return code;
+  return colors[0] * 10 + colors[1];
 }
